e7f2f3f2d3020d903e45d16484e09bf413a9e6cd
Jytw12345 <309953160@qq.com>
2026-09-12 14:31:28 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  *  - 同源静态资源（JS/CSS/图片/字体）：stale-while-revalidate
  *  - 跨域请求（Supabase / COS API 等）不经过缓存，直接走网络
  */
-const CACHE = 'hetong-cache-0.2.25-1789193253960'
+const CACHE = 'hetong-cache-0.2.26-1789194466756'
 const CORE = [
   './index.html',
   './manifest.webmanifest',
